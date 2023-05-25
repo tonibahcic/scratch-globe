e5c3c022530dc987bb5b0b4e4633e423eec02480
Bahčić <toni.bahcic@reev.hr>
2023-05-25 19:09:43 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
+import Globe from "react-globe.gl";
+import geojson from './data/countries.json';
+
+// docs https://www.npmjs.com/package/react-globe.gl#polygons-layer
+// countries https://github.com/vasturiano/react-globe.gl/blob/master/example/datasets/ne_110m_admin_0_countries.geojson
 
 function App() {
+  const [countries, setCountries] = useState<any>({features: []});
+  const [hoverD, setHoverD] = useState();
+
+  useEffect(() => {
+    setCountries(geojson.features)
+    console.log(geojson.features[0])
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Globe
+          // polygonsData={[geojson.features[0]]} // works
+          polygonsData={geojson.features}
+          // globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+          backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+          lineHoverPrecision={0}
+          polygonAltitude={0.01}
+          // polygonCapColor={d => d === hoverD ? 'steelblue' : colorScale(getVal(d))}
+          polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'} // sides
+          polygonStrokeColor={() => '#111'} // borders
+          // polygonLabel={}
+          // onPolygonHover={setHoverD}
+          polygonsTransitionDuration={300}
+        />
       </header>
     </div>
   );
---
+++ src/data/README.md
@@ -0,0 +1,5 @@
+# Data sources
+
+## Countries json
+- Original name and format: `ne_110m_admin_0_countries.geojson`
+- Countries data taken from https://github.com/vasturiano/react-globe.gl/blob/master/example/datasets/ne_110m_admin_0_countries.geojson
