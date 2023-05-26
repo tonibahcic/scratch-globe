dd3b6f32d6129ecf8cca86f16691252d10799c6e
Bahčić <toni.bahcic@reev.hr>
2023-05-26 15:21:54 +0200

    Switch to light more, add geojson type

--- src/components/GlobeWrapper/GlobeWrapper.tsx
+++ src/components/GlobeWrapper/GlobeWrapper.tsx
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from "react";
-import geojson from "../../data/countries.json";
+import countriesJson from "../../data/countries.json";
 import Globe from "react-globe.gl";
 import * as THREE from 'three';
+import {FeatureCollection} from "geojson";
 
 // docs https://www.npmjs.com/package/react-globe.gl#polygons-layer
 // countries https://github.com/vasturiano/react-globe.gl/blob/master/example/datasets/ne_110m_admin_0_countries.geojson
+// geojson https://geojson.org/
+// geojson https://www.npmjs.com/package/@types/geojson
 function GlobeWrapper() {
-  const [countries, setCountries] = useState<any>();
+  const countriesGeoJson: FeatureCollection = countriesJson as unknown as FeatureCollection
   const [countryHovered, setCountryHovered] = useState<any>();
-
-  useEffect(() => {
-    setCountries(geojson.features)
-  }, []);
 
   useEffect(() => {
     console.log(countryHovered?.properties)
@@ -29,27 +28,27 @@ function GlobeWrapper() {
   const globeMaterial = () => {
     let material = new THREE.MeshPhongMaterial()
     let color = new THREE.Color();
-    color.setStyle('#0000ff')
+    color.setStyle('rgba(0,22,161)')
     material.color = color
     material.transparent = true
-    material.opacity = 0.5
+    material.opacity = 0.85
 
     return material
   }
 
   return (
     <Globe
-      polygonsData={countries}
+      polygonsData={countriesGeoJson.features}
       // globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      backgroundColor={'rgb(255,255,255)'}
       lineHoverPrecision={0}
       polygonAltitude={calculateAltitude}
-      // polygonCapColor={d => d === hoverD ? 'steelblue' : colorScale(getVal(d))}
-      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'} // sides
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.0)'} // sides
       polygonStrokeColor={() => '#11409E'} // borders
-      polygonCapColor={() => '#DCEEF8'} // white
+      polygonCapColor={() => 'rgb(154,184,231)'} // country color
       polygonLabel={getLabel}
-      onPolygonHover={ (polygon: object | null, prevPolygon: object | null) =>
+      onPolygonHover={(polygon: object | null, prevPolygon: object | null) =>
         setCountryHovered(polygon)
       }
       polygonsTransitionDuration={300}
